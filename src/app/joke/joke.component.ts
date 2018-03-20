1b243c9de3d629c38282af60d51accb4d86e6650
irfan-synerzip <irfanhanfi06@gmail.com>
2018-03-20 11:48:20 +0530

    String Interpolation

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joke.component.css']
 })
 export class JokeComponent implements OnInit {
-
-  constructor() { }
+  setup: string;
+  punchline: string;
+  constructor() { 
+    this.setup = "What did the cheese say when it looked in the mirror?";
+    this.punchline = "Halloumi (Hello Me)";
+  }
 
   ngOnInit() {
   }
