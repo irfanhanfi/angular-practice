ff5edd557bfcf9c1cd63d0c3813880d946278128
irfan-synerzip <irfanhanfi06@gmail.com>
2018-04-17 10:37:24 +0530

    Hover effect

--- src/app/card-hover.directive.ts
+++ src/app/card-hover.directive.ts
@@ -7,14 +7,11 @@ export class CardHoverDirective {
   @HostBinding('class.card-outline-primary') private ishovering: boolean;
   constructor(private el: ElementRef,
     private renderer: Renderer) {
-    //noinspection TypeScriptUnresolvedVariable,TypeScriptUnresolvedFunction
-    // renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
   }
 
   @HostListener('mouseover') onMouseOver() { 
  
     let part = this.el.nativeElement.querySelector('.card-text') ;
-    // this.renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
     this.renderer.setElementStyle(part, 'display', 'block'); 
     this.ishovering = true;
   }
